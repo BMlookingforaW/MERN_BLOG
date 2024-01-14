f92c9f150c55a61e7a59f88176f9bfa35ce53eb6
Bharat Mishra <99363954+BMlookingforaW@users.noreply.github.com>
2024-01-14 20:05:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,12 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://blog-client-mauve.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 //ROUTES MIDDLEWARE
 app.use('/api', authRoutes);
